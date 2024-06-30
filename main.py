d16ebcf5e533a218f466997d11d84605ab0eb144
TylerHager2 <tylerhager02@outlook.com>
2024-06-30 04:57:19 -0700

    test1

--- main.py
+++ main.py
@@ -5,7 +5,15 @@ def main():
     ########################################
     """
     total = 0
-
+    
+    i = 0
+    
+    while i<5:
+        num = int(input('Enter a number: '))
+        total += num
+        i += 1
+    print('Your total is: ', total)
+    
     ########################################
     # Do not delete the return statement
     ########################################
